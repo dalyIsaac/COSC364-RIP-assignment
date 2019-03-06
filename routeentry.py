2a389845d7d072d402546b84404cc1057fde6deb
dalyIsaac <isaac.daly@outlook.com>
2019-03-07 10:15:24 +1300

    Added `learned_from` to `RouteEntry`.

--- routeentry.py
+++ routeentry.py
@@ -9,23 +9,26 @@ class RouteEntry:
     Entry for a route inside the RIP routing table.
 
     Instance variables:
-    port -- The port for this `RouteEntry`.
+    port -- The output port for this `RouteEntry`.
     next_address - the router-id for the next-address along the path to the destination.
     sched_update_time -- The time at which a normally scheduled Response will be sent to other routers.
     timeout_time -- The time at which the timeout occurs, and the deletion process for this `RouteEntry` starts.
     garbage_collection_time --- The time after which this `RouteEntry` should be deleted from `RoutingTable`.
     flag -- Set to `True` to indicate that the entry has changed.
+    learned_from -- The router-id of the router from whom this route was learned from. -1 if the router wasn't learned 
+    from anyone (i.e. learned from the config file on startup).
     """
 
     flag = False
 
-    def __init__(self, port: int, metric: int, next_address: int):
+    def __init__(self, port: int, metric: int, next_address: int, learned_from = -1):
         self.port = port
         self.metric = metric
         self.next_address = next_address
         current_time = datetime.now()
         self.sched_update_time = current_time + timedelta(SCHEDULED_UPDATE_TIME)
         self.timeout_time = current_time + timedelta(TIMEOUT_TIME)
+        self.learned_from = learned_from
 
     def update_sched_update_time(self, initial_time=datetime.now()) -> datetime:
         """
