b64eb43d9a157a6492f5bee5d9905f5f1601793d
dalyIsaac <isaac.daly@outlook.com>
2019-03-07 09:55:06 +1300

    Added dunder methods, and fixed bug in initializer.

--- routingtable.py
+++ routingtable.py
@@ -1,18 +1,29 @@
 from typing import Dict
-from route import Route
+from routeentry import RouteEntry
 
 
 class RoutingTable:
-    table: Dict[int, Route]
+    table: Dict[int, RouteEntry]
 
     def __init__(self):
-        table = {}
+        self.table = {}
 
-    def add_route(self, router_id: int, route: Route) -> None:
+    def __len__(self):
+        """Returns the length of the routing table"""
+        return len(self.table)
+
+    def __iter__(self):
+        """Iterates over the items in the routing table"""
+        return iter(self.table.keys())
+
+    def __getitem__(self, index: int):
+        """Returns a specific item in the routing table"""
+        return self.table[index]
+
+    def add_route(self, router_id: int, route: RouteEntry) -> None:
         """Adds the route to the table"""
         self.table[router_id] = route
 
     def remove_route(self, router_id: int) -> None:
         """Removes the route from the table."""
         del self.table[router_id]
-
