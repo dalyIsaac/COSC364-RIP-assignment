7aefebd30180a14904359d7267015b64d4440991
dalyIsaac <isaac.daly@outlook.com>
2019-03-20 10:45:28 +1300

    Added code to read a given packet.

--- packet.py
+++ packet.py
@@ -22,10 +22,11 @@ def get_next_packet_entries(table: RoutingTable, router_id: int):
     table -- The routing table, containing all of the entries.
     router_id -- The router the packet is being sent to.
     """
-    entries  = []
+    entries = []
     for destination_router_id in table:
         route = table[destination_router_id]
-        if route.learned_from != router_id and (route.metric < INFINITY or route.flag):
+        if route.learned_from != router_id and (route.metric < INFINITY
+                                                or route.flag):
             entries.append((destination_router_id, route))
             if len(entries) == MAX_ENTRIES:
                 yield entries
@@ -54,9 +55,11 @@ def _construct_packet(table: RoutingTable, entries, router_id) -> bytearray:
     current_index = 4
 
     for (destination_router_id, entry) in entries:
-        packet[current_index : current_index + 2] = AF_INET.to_bytes(2, "big")
-        packet[current_index + 4 : current_index + 8] = destination_router_id.to_bytes(4, "big")
-        packet[current_index + 16 : current_index + ENTRY_LEN] = entry.metric.to_bytes(4, "big")
+        packet[current_index:current_index + 2] = AF_INET.to_bytes(2, "big")
+        packet[current_index + 4:current_index +
+               8] = destination_router_id.to_bytes(4, "big")
+        packet[current_index + 16:current_index +
+               ENTRY_LEN] = entry.metric.to_bytes(4, "big")
         current_index += ENTRY_LEN
 
     return packet
@@ -70,3 +73,31 @@ def construct_packets(table: RoutingTable, router_id: int) -> List[bytearray]:
         packets.append(_construct_packet(table, entries, router_id))
 
     return packets
+
+
+def get_entry(packet: bytearray, start_index: int) -> Tuple[int, int, int]:
+    """Returns the properties of a single RIP entry."""
+    afi = int.from_bytes(packet[start_index:start_index + 2], byteorder="big")
+    target_router_id = int.from_bytes(
+        packet[start_index + 4:start_index + 8], byteorder="big")
+    metric = int.from_bytes(
+        packet[start_index + 16:start_index + 20], byteorder="big")
+    return afi, target_router_id, metric
+
+
+def read_packet(packet: bytearray):
+    """Returns the properties of the received RIP response packet."""
+    command: int = packet[0]
+    version: int = packet[1]
+    sender_router_id: int = int.from_bytes(packet[2:4], byteorder="big")
+
+    entries = []
+    start_index = HEADER_LEN
+
+    while start_index < len(packet):
+        end_index = start_index + ENTRY_LEN
+        if end_index < len(packet):
+            entries.append(get_entry(packet, start_index))
+        else:
+            return command, version, sender_router_id, entries
+        start_index = end_index
--- packet_test.py
+++ packet_test.py
@@ -1,37 +1,69 @@
 from unittest import main, TestCase
 from routingtable import RoutingTable
 from routeentry import RouteEntry
-from packet import construct_packets
+from packet import construct_packets, read_packet
 from socket import AF_INET
 
 
-class TestPacketConstruction(TestCase):
+def get_single_packet():
+    packet = bytearray(24)
+    packet[0] = 2
+    packet[1] = 2
+    packet[2:4] = (0).to_bytes(2, "big")
+    packet[4:6] = AF_INET.to_bytes(2, "big")
+    packet[8:12] = (1).to_bytes(4, "big")
+    packet[20:24] = (1).to_bytes(4, "big")
+    return packet
 
+
+def get_two_packets():
+    packet1 = bytearray(504)
+    packet1[0] = 2
+    packet1[1] = 2
+    packet1[2:4] = (0).to_bytes(2, "big")
+    diff = 1
+    for i in range(25):
+        if i + diff == 2:
+            diff += 1
+        packet1[i * 20 + 4:i * 20 + 6] = AF_INET.to_bytes(2, "big")
+        packet1[i * 20 + 8:i * 20 + 12] = (i + diff).to_bytes(4, "big")
+        packet1[i * 20 + 20:i * 20 + 24] = (1).to_bytes(4, "big")
+
+    packet2 = bytearray(124)
+    packet2[0] = 2
+    packet2[1] = 2
+    packet2[2:4] = (0).to_bytes(2, "big")
+    for i in range(6):
+        packet2[i * 20 + 4:i * 20 + 6] = AF_INET.to_bytes(2, "big")
+        packet2[i * 20 + 8:i * 20 + 12] = (i + 27).to_bytes(4, "big")
+        packet2[i * 20 + 20:i * 20 + 24] = (1).to_bytes(4, "big")
+
+    return packet1, packet2
+
+
+class TestPacketConstruction(TestCase):
     def _test_single_packet(self, packet, expected_packet, iteration=0):
         for i, val in enumerate(expected_packet):
-            self.assertEqual(packet[i], val, f"Iteration {iteration}. Failed on byte {i}. Expected: {val}. Received: {packet[i]}")
-            
+            self.assertEqual(
+                packet[i], val,
+                f"Iteration {iteration}. Failed on byte {i}. Expected: {val}. Received: {packet[i]}"
+            )
+
     def test_single_entry(self):
         """Tests a routing table, where the single entry does not match the given router_id"""
         table = RoutingTable(0)
-        table.add_route(1, RouteEntry(3000, 1, 2)) # port: 3000, metric: 1, next_address: 2
+        table.add_route(1, RouteEntry(
+            3000, 1, 2))  # port: 3000, metric: 1, next_address: 2
 
         packets = construct_packets(table, 3)
 
-        expected_packet = bytearray(24)
-        expected_packet[0] = 2
-        expected_packet[1] = 2
-        expected_packet[2:4] = (0).to_bytes(2, "big")
-        expected_packet[4:6] = AF_INET.to_bytes(2, "big")
-        expected_packet[8:12] = (1).to_bytes(4, "big")
-        expected_packet[20:24] = (1).to_bytes(4, "big")
+        expected_packet = get_single_packet()
 
         self.assertEqual(len(packets), 1)
         packet = packets[0]
         self.assertEqual(len(packet), len(expected_packet))
         self._test_single_packet(packet, expected_packet)
 
-    
     def test_two_entries_router_id_clash(self):
         """
         Tests that a routing table with two entries, where one entry was learnt from the given router_id, only returns 
@@ -57,7 +89,6 @@ class TestPacketConstruction(TestCase):
         self.assertEqual(len(packet), len(expected_packet))
         self._test_single_packet(packet, expected_packet)
 
-
     def test_two_entries_infinity(self):
         """
         Tests that a routing table with two entries, where one entry has a metric of infinity, but is flagged, is 
@@ -75,11 +106,11 @@ class TestPacketConstruction(TestCase):
         expected_packet[1] = 2
         expected_packet[2:4] = (0).to_bytes(2, "big")
         expected_packet[4:6] = AF_INET.to_bytes(2, "big")
-        expected_packet[8:12] = (1).to_bytes(4, "big") #!
+        expected_packet[8:12] = (1).to_bytes(4, "big")
         expected_packet[20:24] = (1).to_bytes(4, "big")
-        expected_packet[20+4:20+6] = AF_INET.to_bytes(2, "big")
-        expected_packet[20+8:20+12] = (2).to_bytes(4, "big")
-        expected_packet[20+20:20+24] = (16).to_bytes(4, "big")
+        expected_packet[20 + 4:20 + 6] = AF_INET.to_bytes(2, "big")
+        expected_packet[20 + 8:20 + 12] = (2).to_bytes(4, "big")
+        expected_packet[20 + 20:20 + 24] = (16).to_bytes(4, "big")
 
         self.assertEqual(len(packets), 1)
         packet = packets[0]
@@ -110,7 +141,7 @@ class TestPacketConstruction(TestCase):
         packet = packets[0]
         self.assertEqual(len(packet), len(expected_packet))
         self._test_single_packet(packet, expected_packet)
-    
+
     def test_multiple_packets(self):
         """
         Tests that multiple packets are returned, when the number of routes is greater than 25.
@@ -123,37 +154,50 @@ class TestPacketConstruction(TestCase):
         table[2].flag = False
         for i in range(30):
             table.add_route(i + 3, RouteEntry(3000, 1, 2))
-        
+
         packets = construct_packets(table, 3)
-
-        expected_packet1  = bytearray(504)
-        expected_packet1[0] = 2
-        expected_packet1[1] = 2
-        expected_packet1[2:4] = (0).to_bytes(2, "big")
-        diff = 1
-        for i in range(25):
-            if i + diff == 2:
-                diff += 1
-            expected_packet1[i*20+4:i*20+6] = AF_INET.to_bytes(2, "big")
-            expected_packet1[i*20+8:i*20+12] = (i + diff).to_bytes(4, "big")
-            expected_packet1[i*20+20:i*20+24] = (1).to_bytes(4, "big")
-
-        expected_packet2 = bytearray(124)
-        expected_packet2[0] = 2
-        expected_packet2[1] = 2
-        expected_packet2[2:4] = (0).to_bytes(2, "big")
-        for i in range(6):
-            expected_packet2[i*20+4:i*20+6] = AF_INET.to_bytes(2, "big")
-            expected_packet2[i*20+8:i*20+12] = (i + 27).to_bytes(4, "big")
-            expected_packet2[i*20+20:i*20+24] = (1).to_bytes(4, "big")
-
-        expected_packets = [expected_packet1, expected_packet2]
+        expected_packets = get_two_packets()
 
         self.assertEqual(len(packets), 2)
         for i, packet in enumerate(packets):
             expected_packet = expected_packets[i]
             self.assertEqual(len(packet), len(expected_packet))
             self._test_single_packet(packet, expected_packet, i)
+
+
+class TestPacketReading(TestCase):
+    def test_single_entry(self):
+        """Tests that a packet with a single entry can be read correctly."""
+        packet = get_single_packet()
+        command, version, sender_router_id, entries = read_packet(packet)
+
+        self.assertEqual(command, 2)
+        self.assertEqual(version, 2)
+        self.assertEqual(sender_router_id, 0)
+        self.assertEqual(len(entries), 1)
+        for afi, target_router_id, metric in entries:
+            self.assertEqual(afi, AF_INET)
+            self.assertEqual(target_router_id, 1)
+            self.assertEqual(metric, 1)
+
+    def test_multiple_entries(self):
+        """
+        Tests that a packet with multiple entries (in this case 25 
+        entries) can correctly read.
+        """
+        packet, _ = get_two_packets()
+        command, version, sender_router_id, entries = read_packet(packet)
+
+        self.assertEqual(command, 2)
+        self.assertEqual(version, 2)
+        self.assertEqual(sender_router_id, 0)
+
+        for i, (afi, target_router_id, metric) in enumerate(entries):
+            expected_target_router_id = i + 2 if i + 1 >= 2 else i + 1
+            self.assertEqual(afi, AF_INET)
+            self.assertEqual(target_router_id, expected_target_router_id)
+            self.assertEqual(metric, 1)
+
 
 if __name__ == '__main__':
     main()
