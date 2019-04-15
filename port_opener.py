d2fa23077718989b84330c8fd0877363ca1c1a80
Manu Francis Hamblyn <mfb31@cs17083jp.canterbury.ac.nz>
2019-04-16 10:20:42 +1200

    Newer version port_opener

---
+++ port_opener.py
@@ -0,0 +1,25 @@
+
+
+import socket
+import sys
+
+def port_opener(ports):
+
+    sockets = None
+    for a_port in (ports)
+
+        in_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        in_socket.setblocking(0)
+        in_socket.bind('localhost', a_port)
+        
+        print ("Opened port, ", in_socket)
+
+        sockets.append(in_socket)
+
+    return (sockets)
+
+if __name__ == "__main__":
+
+    ports = (3001, 4001, 5001)
+    port_opener(ports)
+    
