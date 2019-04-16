adac7e93aeea400b4141a1fda3dd6a6b92c7bc15
Manu Francis Hamblyn <mfb31@cs17083jp.canterbury.ac.nz>
2019-04-16 13:20:36 +1200

    Fixed for formatting errors

--- port_opener.py
+++ port_opener.py
@@ -1,23 +1,23 @@
-
-
 import socket
 import sys
+
 
 def port_opener(ports):
 
     sockets = None
-    
-    for a_port in (ports)
+
+    for a_port in ports:
 
         in_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         in_socket.setblocking(0)
-        in_socket.bind('localhost', a_port)
-        
-        print ("Opened port, ", in_socket)
+        in_socket.bind("localhost", a_port)
+
+        print("Opened port, ", in_socket)
 
         sockets.append(in_socket)
 
-    return (sockets)
+    return sockets
+
 
 if __name__ == "__main__":
 
