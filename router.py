98859b88514f6f7c2e7ab8358cd640b4d60d4d8c
dalyIsaac <isaac.daly@outlook.com>
2019-04-18 10:18:13 +1200

    Added socket parameters to the necessary functions

--- router.py
+++ router.py
@@ -9,11 +9,18 @@ from routingtable import RoutingTable
 
 def daemon(table: RoutingTable, sockets: List[socket]):
     """Main body of the router."""
+    if len(sockets) == 0:
+        print("No sockets were opened.")
+        return
+
+    # first open socket is chosen to be the output socket
+    output_sock: socket = sockets[0]
+
     while True:
         while table.sched_update_time < datetime.now():
             input_processing(table, sockets)
 
-        send_responses(table)
+        send_responses(table, output_sock)
         table.update_sched_update_time()
 
-        deletion_process(table)
+        deletion_process(table, output_sock)
