d4d641d5f46027f2537e229d51d86ebc647c8634
Manu Francis Hamblyn <mfb31@cs17083jp.canterbury.ac.nz>
2019-04-17 15:13:53 +1200

    Update port_opener, tidying validate data

--- port_opener.py
+++ port_opener.py
@@ -1,25 +1,59 @@
+#########################################################
+#
+# A function to open / bind ports to sockets
+#
+# Version 01: March 2019
+#   First pass
+#  
+# Version 02: 17 April 2019
+#   change to match output port with destination router  
+#   ID
+# 
+# Version 03: 17 April 2019
+#   updated to take in the complete "output_list" tuple  
+#   check if id is null:
+#   YES => return tuple of sockets (input ports)
+#   NO => return tuple of sockets (output ports) and
+#   destination router ID
+#                                         
+#########################################################
+
 import socket
 import sys
 
+def port_opener(output_ports):
 
-def port_opener(ports):
+    socket_list = []
 
-    sockets = None
+    if (output_ports[2] == None):
+        for a_port,a_cost,an_id in output_ports:
 
-    for a_port in ports:
+            new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            new_socket.setblocking(0)
+            new_socket.bind("localhost", a_port)
 
-        in_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        in_socket.setblocking(0)
-        in_socket.bind("localhost", a_port)
+            print("Opened socket / port, ", new_socket, "/", a_port)
 
-        print("Opened port, ", in_socket)
+            socket_list.append((new_socket))
 
-        sockets.append(in_socket)
+        return (socket_list)
 
-    return sockets
+    else:
+        for a_port,a_cost,an_id in output_ports:
+
+            new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            new_socket.setblocking(0)
+            new_socket.bind("localhost", a_port)
+
+            print("Opened socket / port / ID, ", new_socket, "/", a_port, "/", an_id)
+
+            socket_list.append((new_socket, an_id))
+
+        return (socket_list)
 
 
 if __name__ == "__main__":
 
-    ports = (3001, 4001, 5001)
-    port_opener(ports)
+    output_ports = ( (3001, 4001, 5001),(1,2,3),(3,4,5) )
+    
+    port_opener(output_ports)
