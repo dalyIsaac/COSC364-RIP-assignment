35f067569003df04774f3d9e9d15f9cf412922f3
Manu Francis Hamblyn <mfb31@cs17083jp.canterbury.ac.nz>
2019-04-17 15:41:16 +1200

    added try catch to port_opener

--- port_opener.py
+++ port_opener.py
@@ -9,12 +9,17 @@
 #   change to match output port with destination router  
 #   ID
 # 
-# Version 03: 17 April 2019
+# Version 03:
 #   updated to take in the complete "output_list" tuple  
 #   check if id is null:
 #   YES => return tuple of sockets (input ports)
 #   NO => return tuple of sockets (output ports) and
 #   destination router ID
+# 
+# Version 04: 
+#   need a try catch exception handler, in case a port
+#   did not open/bind which returns am error code so
+#   main can close everything and exit
 #                                         
 #########################################################
 
@@ -25,35 +30,37 @@ def port_opener(output_ports):
 
     socket_list = []
 
-    if (output_ports[2] == None):
-        for a_port,a_cost,an_id in output_ports:
+    try:
+        if (output_ports[2] == None):
+            for a_port,a_cost,an_id in output_ports:
 
-            new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            new_socket.setblocking(0)
-            new_socket.bind("localhost", a_port)
+                new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                new_socket.setblocking(0)
+                new_socket.bind("localhost", a_port)
 
-            print("Opened socket / port, ", new_socket, "/", a_port)
+                print("Opened socket / port, ", new_socket, "/", a_port)
 
-            socket_list.append((new_socket))
+                socket_list.append((new_socket))
 
-        return (socket_list)
+            return (socket_list)
 
-    else:
-        for a_port,a_cost,an_id in output_ports:
+        else:
+            for a_port,a_cost,an_id in output_ports:
 
-            new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            new_socket.setblocking(0)
-            new_socket.bind("localhost", a_port)
+                new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                new_socket.setblocking(0)
+                new_socket.bind("localhost", a_port)
 
-            print("Opened socket / port / ID, ", new_socket, "/", a_port, "/", an_id)
+                print("Opened socket / port / ID, ", new_socket, "/", a_port, "/", an_id)
 
-            socket_list.append((new_socket, an_id))
+                socket_list.append((new_socket, an_id))
 
-        return (socket_list)
-
+            return (socket_list)
+    except:
+        return(1)
 
 if __name__ == "__main__":
 
+    # ouput ports, cost, destination id
     output_ports = ( (3001, 4001, 5001),(1,2,3),(3,4,5) )
-    
     port_opener(output_ports)
