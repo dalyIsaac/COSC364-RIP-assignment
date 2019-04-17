0f9e30615d375ad7377555fdda1cab6610be7cbc
dalyIsaac <isaac.daly@outlook.com>
2019-04-17 17:23:54 +1200

    Added initial structure to update the table

--- input_processing.py
+++ input_processing.py
@@ -3,6 +3,7 @@ from packet import validate_packet, ResponsePacket, ResponseEntry
 from validate_data import MIN_ID, MAX_ID, INFINITY
 from typing import List, Tuple
 from routeentry import RouteEntry
+from datetime import datetime, timedelta
 
 MIN_METRIC = 1
 MAX_METRIC = INFINITY
@@ -45,6 +46,43 @@ def add_route(
     entry.flag = True
     table.add_route(response.router_id, entry)
     # TODO: trigger an update
+
+
+def update_table(
+    table: RoutingTable,
+    response: ResponseEntry,
+    new_metric: int,
+    learned_from: int,
+):
+    entry: RouteEntry = table[response.router_id]
+
+    if (
+        learned_from == entry.next_address and new_metric != entry.metric
+    ) or new_metric < entry.metric:
+        entry.metric = new_metric
+        entry.next_address = learned_from
+        entry.flag = True
+        # TODO: trigger an update
+        if new_metric == INFINITY:
+            # TODO: start the deletion process
+            pass
+        else:
+            entry.update_timeout_time(table.timeout_delta)
+    elif new_metric == INFINITY:
+        if entry.metric != INFINITY:
+            # TODO: start the deletion process
+            pass
+    elif new_metric == entry.metric:
+        # if the timeout for the existing route is at least halfway to the
+        # expiration point, switch to the new route.
+        time_diff: timedelta = entry.timeout_time - datetime.now()
+        half_time = table.timeout_delta / 2
+        if time_diff.seconds >= half_time:
+            # TODO: switch to the new route
+            pass
+    else:
+        # Drop all the remaining packets
+        pass
 
 
 def process_entry(
