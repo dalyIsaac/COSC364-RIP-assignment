c7b8b43146af0613d91c66a5e1e47752ac453632
dalyIsaac <isaac.daly@outlook.com>
2019-05-09 11:27:17 +1200

    Updated validation for the config file - Switched to using Booleans - Exits faster and prints out the correct error, if router-id is not present - Added code in case input-ports isn't present

--- validate_data.py
+++ validate_data.py
@@ -70,43 +70,40 @@ PERIODIC_GARBAGE_RATIO = 4
 
 
 def validate_data(router_id, input_ports, output_ports, timers):
-    id_error = 0
-    input_port_error = 0
-    output_port_error = 0
-    metric_error = 0
-    timers_error = 0
-    # temp_metric_list = None
-    # temp_id_list = None
-    # temp_metric_list = None
+    id_error = False
+    input_port_error = False
+    output_port_error = False
+    metric_error = False
+    timers_error = False
 
     # Check Router ID
     if router_id is None:
-        id_error = 1
+        id_error = True
     else:
         if (router_id < MIN_ID) or (router_id > MAX_ID):
-            id_error = 1
+            id_error = True
 
-        if id_error == 1:
-            logger("Router ID Configuration Error")
-            return False
+    if id_error:
+        logger("Router ID Configuration Error")
+        return False
 
     # Check input ports
     temp_input_set = set()
     if input_ports is None:
-        input_port_error = 1
+        input_port_error = True
     else:
         for a_port in input_ports:
             if (a_port < MIN_PORT) or (a_port > MAX_PORT):
-                input_port_error = 1
+                input_port_error = True
                 break
             temp_input_set.add(a_port)  # add port to a temporary list
 
     # set error error if the length of temporary list
     #   is not the same as length of original port list
-    if len(temp_input_set) != len(input_ports):
-        input_port_error = 1
+    if input_ports is None or len(temp_input_set) != len(input_ports):
+        input_port_error = True
 
-    if input_port_error == 1:
+    if input_port_error:
         logger("Input Ports Configuration Error")
         return False
 
@@ -129,52 +126,52 @@ def validate_data(router_id, input_ports, output_ports, timers):
 
             # check metric
             if (an_item[1] < MIN_METRIC) or (an_item[1] > MAX_METRIC):
-                metric_error = 1
+                metric_error = True
                 break
             # temp_metric_set.add(an_item[1])
 
             # check id
             if (an_item[2] < MIN_ID) or (an_item[2] > MAX_ID):
-                id_error = 1
+                id_error = True
                 break
             temp_id_set.add(an_item[2])
 
     # if length of set of ports not same as the length of output-ports, hv error
     if len(temp_output_port_set) != len(output_ports):
-        output_port_error = 1
+        output_port_error = True
 
     # check the set of output ports with set of input ports, if they
     # are not disjoint (i.e. element(s) in common) we have error
     if (temp_output_port_set.isdisjoint(temp_input_set)) is False:
-        output_port_error = 1
+        output_port_error = True
 
-    if metric_error == 1:
+    if metric_error:
         logger("Output Ports Configuration Error: Cost / Metric")
         return False
 
     # we might need this i.e. check if a cost / metric is missing?
     # if len(temp_metric_set) != len(output_ports(2)):
-    # id_error = 1
+    # id_error = True
 
     # do we need a check for missing ID?
-    if id_error == 1:
+    if id_error:
         logger("Output Ports Configuration Error: ID")
         return False
 
-    if output_port_error == 1:
+    if output_port_error:
         logger("Output Ports Configuration Error: Port number re-use")
         return False
 
     # Check Timers
     if len(timers) != 3:
-        timers_error = 1
+        timers_error = True
     else:
         if (timers[1] / timers[0]) != PERIODIC_DEAD_RATIO:
-            timers_error = 1
+            timers_error = True
         if (timers[2] / timers[0]) != PERIODIC_GARBAGE_RATIO:
-            timers_error = 1
+            timers_error = True
 
-    if timers_error == 1:
+    if timers_error:
         logger("Timers Configuration Error")
         return False
 
